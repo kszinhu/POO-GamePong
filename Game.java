6a3603f031d7c05a1e0c1364c897733c5a716924
Kszinhu <rodrigues.cassiano2007@gmail.com>
2021-06-12 01:39:04 -0300

    ✏️ Edit game window

--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Game extends JFrame {
@@ -8,14 +9,15 @@ public class Game extends JFrame {
 
   public Game() {
     add(new World());
-    setTitle("SMB");
-    setSize(1024, 768);
+    setTitle("Space inPOO");
+    setSize(1024, 1024);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(null);
     this.setResizable(false);
+    setIconImage(new ImageIcon("icon.png").getImage());
+
     setVisible(true);
   }
-
   public static void main(String[] args) {
     new Game();
   }
